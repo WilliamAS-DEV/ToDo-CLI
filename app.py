e3aed78346e0fa8b4214a9227386156761dd535c
WilliamAS-DEV <aftonskywalker@gmail.com>
2026-01-06 22:39:22 +0000

    Incomplete version

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+# 1. Importar as bibliotecas necessarias
+import json
+import argparse
+import os
 
+# 2. Abrir o arquivo de tarefas; Se não existe, crie ele
+if not os.path.exists('./src/'):
+    os.makedirs('./src/')
+    with open('./src/tasks.json', 'w') as f:
+        json.dump([], f, indent=4)
+elif not os.path.exists("./src/tasks.json"):
+    with open('./src/tasks.json', 'w') as f:
+        json.dump([], f, indent=4)
+else:
+    pass
+
+with open('./src/tasks.json', 'r') as f:
+    tasks = json.load(f)
+    
+# 3. Classes e Funções
+class ToDo:
+    def list():
+        print("ID     Name     Date")
+        for i in tasks["tasks"]:
+            print(f"{i}    {i['name']}    {i['dh']}")
+# 4. Configurar o argparse
+
+print(tasks)
